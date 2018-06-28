aa735849a8a59c5e0e17fb8ae1f67eca8f4667d9
Michael Dastrup <michaelmaxdastrup@gmail.com>
2018-06-28 15:02:59 -0600

    styled clock with CSS Grid and detailed styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import Picker from './picker';
+import Button from './button';
+import Clock from './clock';
 
 export default class App extends Component {
   render() {
+
+    //return <div className=""><Clock/></div>
+
     return (
       <div className="grid">
         <h1 className="grid__title">Birthday Countdown</h1>
@@ -17,6 +22,7 @@ export default class App extends Component {
 
         <Picker/>
         { Button('Generate Countdown') }
+        <Clock/>
       </div>
     );
   }
